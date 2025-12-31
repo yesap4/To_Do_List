0ebfaa8d40155b9330cf9eabc9617ee24c8b60f7
Anant Prakash <yes4ap@gmail.com>
2025-12-31 09:26:53 +0000

    Implement basic To-Do List functionality with task addition and removal

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import streamlit as st
 
+
+st.title('To-Do List')
+
+task = st.text_input('Enter Your Task:', '')
+
+if st.button('Add Task'):
+    if task:
+        st.session_state['task_list'].append(task)
+
+
+
+
+if 'task_list' not in st.session_state:
+    st.session_state['task_list'] = []
+
+
+for i, t in enumerate(st.session_state['task_list']):
+    st.write(f'{i + 1}. {t}')
+
+
+for i, t in enumerate(st.session_state['task_list']):
+    if st.checkbox(f'{i + 1}. Remove Task {i + 1}'):
+        st.session_state['task_list'].remove(t)
